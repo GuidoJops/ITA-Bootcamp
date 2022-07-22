f61f787b8af1e6682d0689e946c90b04747ee7ee
GuidoJ <guidojanota@gmail.com>
2022-07-22 02:05:31 +0200

    1era Version

---
+++ Nivel 1/src/s1t3e1/Mes.java
@@ -0,0 +1,14 @@
+package s1t3e1;
+
+public class Mes {
+	private String nombre;
+
+	public Mes(String nombre) {
+		this.nombre = nombre;
+		
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+}
