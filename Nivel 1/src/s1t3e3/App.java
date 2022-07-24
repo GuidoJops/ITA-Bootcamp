3128f0f064069d416e497ab3b0fccc49d17da850
GuidoJ <guidojanota@gmail.com>
2022-07-24 11:36:09 +0200

    v1

--- Nivel 1/src/s1t3e3/App.java
+++ Nivel 1/src/s1t3e3/App.java
@@ -1,7 +1,10 @@
 package s1t3e3;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -16,21 +19,22 @@ import java.util.Scanner;
 public class App {
 
 	public static void main(String[] args) {
-		String user;
+		String user="";
 		FileReader fl;
 		BufferedReader br;
 		String line;
 		String [] kv;
-		//int cont=0;
+		int numberCountries=0, score;
 		
 		HashMap<String, String> countries = new HashMap<String,String>();
 		
 		
-//		user = ingresaStr("Dime tu nombre")
+		user = ingresaStr("Dime tu nombre");
 		
 		try {
 			fl = new FileReader("src/s1t3e3/Countries.txt");
 			br = new BufferedReader(fl);
+			System.out.println("Lectura de fichero Exitosa!");
 			
 			do {
 				line = br.readLine();
@@ -38,12 +42,14 @@ public class App {
 					kv = line.split(" ");
 					countries.put(kv[0],kv[1]);
 					//System.out.println(kv[0]);
-					//cont+=1;
+					numberCountries+=1;
 //					System.out.println(kv[1]);
 
 				}
 				
 			}while(line!=null);	
+			
+			br.close();	//Cierro Buffer
 			
 		} catch (Exception e) {
 			System.out.println("Ha ocurrido un error.\nVerifique la ruta del fichero.");
@@ -51,12 +57,31 @@ public class App {
 			
 		for (String key:countries.keySet()) {
 	    String value = countries.get(key);
-	    System.out.println( key +"-->"+ value);
-	}
-		//System.out.println(cont);
-		juego(countries);
+//	    System.out.println( key +"-->"+ value);
+		}
+		System.out.println(numberCountries);
+		score = juego(countries,numberCountries);
+		escribeFichero(user, score);
 	
 		System.out.println("\nFIN DEL PROGRAMA");
+	}
+	
+	static void escribeFichero(String usr, int score) {
+		try {
+			String line;
+			FileWriter wr=new FileWriter("src/s1t3e3/Puntajes.txt", true); //No se sobrescribe el fichero
+			BufferedWriter bw = new BufferedWriter(wr);
+			
+			bw.write(usr + "---> "+ score+"\n");
+			
+			System.out.println("Se ha cargado el puntaje en el fichero externo");
+			
+			bw.close();	//Cierro Buffer
+			
+		} catch (IOException e) {
+			
+			System.out.println("Ups..Algo ha fallado al intentar escribir el fichero.");
+		}
 	}
 	
 
@@ -66,25 +91,26 @@ public class App {
 		return input.nextLine();
 	}
 
-	static void juego(HashMap<String, String> countries) {
-		int puntaje = 0;
-		for(int i=10; i>0;i--) {
-			int randomNumber = (int) (Math.floor(Math.random() * (51+1)));
+	static int juego(HashMap<String, String> countries, int numberCountries) {
+		int score = 0, randomNumber;
+		for(int i=3; i>0;i--) {
+			
+			randomNumber = (int) (Math.floor(Math.random() * (numberCountries)));; //A veces da el mismo Nº
 			Object key = countries.keySet().toArray()[randomNumber];
 			Object value = countries.get(key).toUpperCase();
-			String str = ingresaStr("Acierta la Captial de "+ key+"...");
+			String str = ingresaStr("Acierta la Captial de "+ key+"..." + randomNumber);
 			System.out.println(value);
 			if (compruebaAcierto(value, str)) {
-				puntaje++;
+				score++;
 			}
 			
 		}
 	
-		System.out.println(puntaje);
+		return score;
 		
 	}
 	
 	static boolean compruebaAcierto(Object val, String str) {
-		return (val.equals(str.toUpperCase()));
+		return (val.equals(str.toUpperCase().replace(" ", "_")));
 	}
 }
