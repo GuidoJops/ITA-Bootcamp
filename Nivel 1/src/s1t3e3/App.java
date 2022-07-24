9a4d2808d8f06f96c128e4e56e83d7adbcc9af49
GuidoJ <guidojanota@gmail.com>
2022-07-24 13:30:12 +0200

    v2

--- Nivel 1/src/s1t3e3/App.java
+++ Nivel 1/src/s1t3e3/App.java
@@ -1,13 +1,3 @@
-package s1t3e3;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Scanner;
-
 /*
  Donat el fitxer countrties.txt (revisa l'apartat recursos) que conté països i capitals. 
  El programa ha de llegir el fitxer i guardar les dades en un HashMap<String, String>. 
@@ -16,54 +6,76 @@ import java.util.Scanner;
  Si l’encerta se li suma un punt. Aquesta acció es repeteix 10 vegades. Un cop demanades les capitals de 10 països 
  de forma aleatòria, llavors s’ha de guardar en un fitxer anomenat classificacio.txt, el nom de l’usuari/ària i la seva puntuació.
  */
+package s1t3e3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
 public class App {
+	static int numberCountries=0, index=-1;
+	static HashMap<String, String> countries = new HashMap<String,String>();
+	static List<Integer> randomNumbers = new ArrayList<>();
+	
 
 	public static void main(String[] args) {
-		String user="";
+		int score;
+		String user = ingresaStr("Dime tu nombre");		
+		leeFichero();
+		
+		
+		//GENERO LISTA DE NUMEROS RANDOM (pasar a funcion??)
+		for (int i = 0; i < numberCountries; i++) {
+			randomNumbers.add(i);
+		}
+		Collections.shuffle(randomNumbers);
+		
+		
+		
+			
+		score = evaluaCantAciertos(numberCountries);
+		escribeFichero(user, score);
+		
+		
+	
+		System.out.println("\nFIN DEL PROGRAMA");
+	}
+	
+	
+	//LEE FICHEROS Y AGREGA CONTENIDO A HASHMAP
+	static void leeFichero() {
 		FileReader fl;
 		BufferedReader br;
 		String line;
 		String [] kv;
-		int numberCountries=0, score;
-		
-		HashMap<String, String> countries = new HashMap<String,String>();
-		
-		
-		user = ingresaStr("Dime tu nombre");
 		
 		try {
 			fl = new FileReader("src/s1t3e3/Countries.txt");
 			br = new BufferedReader(fl);
-			System.out.println("Lectura de fichero Exitosa!");
+			System.out.println("Lectura de fichero Exitosa!\n");
 			
 			do {
 				line = br.readLine();
 				if(line!=null) {
 					kv = line.split(" ");
 					countries.put(kv[0],kv[1]);
-					//System.out.println(kv[0]);
 					numberCountries+=1;
-//					System.out.println(kv[1]);
-
 				}
 				
 			}while(line!=null);	
 			
-			br.close();	//Cierro Buffer
+			br.close();			//Cierro Buffer
 			
 		} catch (Exception e) {
 			System.out.println("Ha ocurrido un error.\nVerifique la ruta del fichero.");
 		}
-			
-		for (String key:countries.keySet()) {
-	    String value = countries.get(key);
-//	    System.out.println( key +"-->"+ value);
-		}
-		System.out.println(numberCountries);
-		score = juego(countries,numberCountries);
-		escribeFichero(user, score);
-	
-		System.out.println("\nFIN DEL PROGRAMA");
 	}
 	
 	static void escribeFichero(String usr, int score) {
@@ -74,7 +86,9 @@ public class App {
 			
 			bw.write(usr + "---> "+ score+"\n");
 			
-			System.out.println("Se ha cargado el puntaje en el fichero externo");
+			System.out.println(usr + " ha conseguido: "+ score + " puntos.");
+			
+			System.out.println("\nSe ha cargado el puntaje en el fichero externo");
 			
 			bw.close();	//Cierro Buffer
 			
@@ -84,22 +98,21 @@ public class App {
 		}
 	}
 	
-
 	static String ingresaStr(String str) {		
 		Scanner input = new Scanner(System.in);
 		System.out.println(str);
 		return input.nextLine();
 	}
 
-	static int juego(HashMap<String, String> countries, int numberCountries) {
-		int score = 0, randomNumber;
-		for(int i=3; i>0;i--) {
-			
-			randomNumber = (int) (Math.floor(Math.random() * (numberCountries)));; //A veces da el mismo Nº
-			Object key = countries.keySet().toArray()[randomNumber];
+	static int evaluaCantAciertos(int numberCountries) {
+		int score = 0;
+		
+		for(int i=10; i>0;i--) {
+			Object key = countries.keySet().toArray()[numRandom()];
 			Object value = countries.get(key).toUpperCase();
-			String str = ingresaStr("Acierta la Captial de "+ key+"..." + randomNumber);
-			System.out.println(value);
+			String str = ingresaStr("Acierta la Captial de "+ key+"...");
+//			System.out.println(value); 			//Respuesta correcta
+			
 			if (compruebaAcierto(value, str)) {
 				score++;
 			}
@@ -113,4 +126,13 @@ public class App {
 	static boolean compruebaAcierto(Object val, String str) {
 		return (val.equals(str.toUpperCase().replace(" ", "_")));
 	}
+
+	static int numRandom() {
+		index++;
+//		System.out.println(randomNumbers.get(index));
+		return randomNumbers.get(index);
+		
+		
+	}
+
 }
