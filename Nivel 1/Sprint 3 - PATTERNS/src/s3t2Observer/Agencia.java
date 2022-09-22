e6feb4f6c73ccb103c514494ab31cc4646b7c7a3
GuidoJ <guidojanota@gmail.com>
2022-09-22 20:20:13 +0200

    S3T2

--- Nivel 1/Sprint 3 - PATTERNS/src/s3t2Observer/Agencia.java
+++ Nivel 1/Sprint 3 - PATTERNS/src/s3t2Observer/Agencia.java
@@ -2,15 +2,15 @@ package s3t2Observer;
 
 public class Agencia extends Observer {
 	
-	public Agencia(Sujeto agenteBolsa, String nombre) {
-		this.agenteBolsa = agenteBolsa;
-		this.agenteBolsa.agregar(this);
+	public Agencia(Sujeto bolsa, String nombre) {
+		this.bolsa = bolsa;
+		this.bolsa.agregar(this);
 		this.nombre = nombre;
 	}
 	
 	@Override
 	public void actualizar() {
-		if(agenteBolsa.getEstado()) {
+		if (bolsa.getEstado()) {
 			System.out.println(nombre + " notificada-> La bolsa SUBE");
 		} else {
 			System.out.println(nombre + " notificada-> La bolsa BAJA");
--- Nivel 1/Sprint 3 - PATTERNS/src/s3t2Observer/Observer.java
+++ Nivel 1/Sprint 3 - PATTERNS/src/s3t2Observer/Observer.java
@@ -3,7 +3,7 @@ package s3t2Observer;
 public abstract class Observer {
 	
 	String nombre;
-	Sujeto agenteBolsa;
+	Sujeto bolsa;
 	public abstract void actualizar();
 
 }
