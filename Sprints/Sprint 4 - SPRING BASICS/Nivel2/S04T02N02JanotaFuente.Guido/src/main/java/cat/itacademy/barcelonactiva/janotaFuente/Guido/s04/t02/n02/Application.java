ad9b25e8f848a4c61ef990a8cad40c60068ff4f7
GuidoJ <guidojanota@gmail.com>
2023-01-07 22:52:39 +0100

    S4T02-N2-inicio

---
+++ Sprints/Sprint 4 - SPRING BASICS/Nivel2/S04T02N02JanotaFuente.Guido/src/main/java/cat/itacademy/barcelonactiva/janotaFuente/Guido/s04/t02/n02/Application.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.janotaFuente.Guido.s04.t02.n02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ Sprints/Sprint 4 - SPRING BASICS/Nivel2/S04T02N02JanotaFuente.Guido/src/test/java/cat/itacademy/barcelonactiva/janotaFuente/Guido/s04/t02/n02/ApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.janotaFuente.Guido.s04.t02.n02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
